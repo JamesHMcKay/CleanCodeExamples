9d93b9f7c6eff14cab6890f1c862d8f50c679d4c
James McKay <james.mckay@stats.govt.nz>
2020-10-21 10:28:19 +1300

    updates

--- R/plot_examples.R
+++ R/plot_examples.R
@@ -2,3 +2,11 @@ load_years_at_usual_residence <- function() {
   data <- load_data("data/years-at-usual-residence-2018-census-csv.csv", col_names = c("code","years_at","count"))
   return(data)
 }
+
+convert_year_to_numbers <- function(data) {
+  data <- data %>%
+    mutate(
+      year_at = as.numeric(gsub("years", "", years_at))
+    )
+  return (data)
+}
